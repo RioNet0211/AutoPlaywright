55ecec0a40aa916bb59ccc415026e2e3473b441f
QATest <yihsu@QATest>
2025-08-17 23:24:21 -0700

    bugfix

--- electron/testbase.js
+++ electron/testbase.js
@@ -6,21 +6,16 @@ const electronTest = base.extend({
   // It will run for each test that uses it.
   electronApp: [async ({}, use) => {    
     // 'use' provides the launched app instance to the test.
-    console.log(`base.extend::electronApp${_electronApp}`);
-    await use(_electronApp);    
+    console.log(`base.extend::electronApp${!!global.__ELECTRON_APP__}`);
+    await use(global.__ELECTRON_APP__);    
   }, { scope: 'test' }],
 
   // This fixture depends on 'electronApp' and provides the main window as 'page'.
   page: [async ({}, use) => {
-    console.log(`inside of base.extend, ${!!_appWindow}`);
-
-    await use(_appWindow);
+    console.log(`inside of base.extend, ${!!global.__APP_WINDOW__}`);
+    await use(global.__APP_WINDOW__);
   }, { scope: 'test' }],
 });
 
-<<<<<<< HEAD
-=======
-// Export the custom test object and expect assertion.
->>>>>>> 72db10ef4a670110cd03fde176b386d6686f6428
 module.exports = { test: electronTest, expect };
 
